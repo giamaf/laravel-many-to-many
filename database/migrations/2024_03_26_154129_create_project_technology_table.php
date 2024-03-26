23ee52377dff64a5e144deeb2c66daa59dc7e60e
giamaf <gianluca.maffucci@hotmail.com>
2024-03-26 15:50:18 +0100

    make bridge table (project_technology)

---
+++ database/migrations/2024_03_26_154129_create_project_technology_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Project;
+use App\Models\Technology;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_technology', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(Project::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Technology::class)->constrained()->onDelete('cascade');
+
+            // Info che non serve necessariamente
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_technology');
+    }
+};
